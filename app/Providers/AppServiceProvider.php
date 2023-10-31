6fc3e2f50caf549cbfbf3fe3af3596db02a3c8c3
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-10-31 20:39:24 +0100

    moved faker and added to the boot method in app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Model::unguard(); 
+        Model::unguard();
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }

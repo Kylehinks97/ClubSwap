374e32d3cd60057687939b951fd3d3556cd5fce6
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-11-01 15:25:07 +0100

    made the controller just say hi for now

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Kreait\Firebase\Factory;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ListingController;
 
-use Kreait\Firebase\Factory;
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\ConversationController;
 
 /*
 |--------------------------------------------------------------------------

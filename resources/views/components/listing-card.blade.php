54b5d616317a1112e382c4787cc096d112a27ca4
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-11-01 12:36:03 +0100

    update path to get image path from the table

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -4,7 +4,7 @@
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            src="{{ $listing->images ? asset('storage/' . $listing->images) : asset('/images/no-image.png')}}"
+            src="{{ $listing->images ? $listing->images : asset('/images/no-image.png')}}"
             alt="" />
         <div>
             <h3 class="text-2xl">
